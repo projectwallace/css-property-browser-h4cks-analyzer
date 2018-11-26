93c1cb2d6d3497e4f074e9d4c05613cad0d77061
Bart Veneman <1536852+bartveneman@users.noreply.github.com>
2018-11-26 21:30:57 +0100

    Fixes issue where -khtml- was considered a browser hack (#2)
    Also restructures the tests to cover a couple more cases.

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
-module.exports = property => {
+const vendorPrefixes = require('vendor-prefixes')()
+
+function startsWithHackCharacter(property) {
 	// A property that starts with any of these characters is
 	// considered a browser hack
-	const startCharacters = '_ ! $ & * ( ) = % + @ , . / ` [ ] # ~ ? : < > |'.split(
+
+	const startCharacters = '_ ! $ & * ( ) = % + @ , . / ` [ ] # ~ ? : < > | -'.split(
 		' '
 	)
 
-	// This regex asserts that a property starts with a `-`
-	// but is not immediately followed by another `-` or any
-	// of the common browser prefixes
-	const browserPrefixRegex = /^-(?!-|webkit-|moz-|ms-|o-)/
+	return startCharacters.some(character => property.startsWith(character))
+}
 
+function isVendorPrefixed(property) {
+	return vendorPrefixes.some(prefix => property.startsWith(prefix))
+}
+
+function isCustomProperty(property) {
+	return /^--[a-z]+/i.test(property)
+}
+
+module.exports = property => {
 	return (
-		startCharacters.some(character => property.startsWith(character)) ||
-		browserPrefixRegex.test(property)
+		startsWithHackCharacter(property) &&
+		!isVendorPrefixed(property) &&
+		!isCustomProperty(property)
 	)
 }
