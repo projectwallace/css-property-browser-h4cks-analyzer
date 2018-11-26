d5dd57bbbc2408145dc09a64973927ff787339a3
Bart Veneman <bartveneman@users.noreply.github.com>
2018-11-26 21:27:04 +0100

    Fixes issue where -khtml- was considered a browser hack

--- test.js
+++ test.js
@@ -1,39 +1,59 @@
 const test = require('ava')
 const isBrowserHack = require('.')
 
-test('It recognizes browser hacks successfully', t => {
-	t.true(isBrowserHack('!property'))
-	t.true(isBrowserHack('$property'))
-	t.true(isBrowserHack('&property'))
-	t.true(isBrowserHack('*property'))
-	t.true(isBrowserHack(')property'))
-	t.true(isBrowserHack('=property'))
-	t.true(isBrowserHack('%property'))
-	t.true(isBrowserHack('+property'))
-	t.true(isBrowserHack('@property'))
-	t.true(isBrowserHack(',property'))
-	t.true(isBrowserHack('.property'))
-	t.true(isBrowserHack('/property'))
-	t.true(isBrowserHack('`property'))
-	t.true(isBrowserHack(']property'))
-	t.true(isBrowserHack('#property'))
-	t.true(isBrowserHack('~property'))
-	t.true(isBrowserHack('?property'))
-	t.true(isBrowserHack(':property'))
-	t.true(isBrowserHack('|property'))
-	t.true(isBrowserHack('_property'))
-	t.true(isBrowserHack('-property'))
-	t.true(isBrowserHack('-opacity'))
-	t.true(isBrowserHack('(;property'))
-	t.true(isBrowserHack('[;property'))
-	t.true(isBrowserHack('-PROPERTY'))
+const hacks = [
+	'!property',
+	'$property',
+	'&property',
+	'*property',
+	')property',
+	'=property',
+	'%property',
+	'+property',
+	'@property',
+	',property',
+	'.property',
+	'/property',
+	'`property',
+	']property',
+	'#property',
+	'~property',
+	'?property',
+	':property',
+	'|property',
+	'_property',
+	'-property',
+	'(;property',
+	'[;property',
+	'-opacity',
+	'-----test'
+]
+
+test('it recognizes browser hacks successfully', t => {
+	t.plan(hacks.length)
+
+	hacks.map(hack => t.true(isBrowserHack(hack)))
 })
 
-test('It correctly marks regular properties as non-hacks', t => {
+test('it handles casing correctly for hacks', t => {
+	t.true(isBrowserHack('-PROPERTY'))
+	t.true(isBrowserHack('-OPACITY'))
+})
+
+test('it does not mark regular properties as hacks', t => {
 	t.false(isBrowserHack('color'))
-	t.false(isBrowserHack('-webkit-transition'))
-	t.false(isBrowserHack('-moz-transition'))
-	t.false(isBrowserHack('-o-transition'))
+	t.false(isBrowserHack('background-image'))
+})
+
+test('it does not mark custom properties as hacks', t => {
 	t.false(isBrowserHack('--my-custom-property'))
 	t.false(isBrowserHack('--My-Custom-Property'))
+	t.false(isBrowserHack('--bgColor'))
+})
+
+test('it does not mark vendor prefixed properties as hacks', t => {
+	t.false(isBrowserHack('-webkit-transition'))
+	t.false(isBrowserHack('-moz-transition'))
+	t.false(isBrowserHack('-khtml-transition'))
+	t.false(isBrowserHack('-o-transition'))
 })
